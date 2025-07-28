cd0f5ed5b6f87f77d229ba2a4f0ccc68bfe1e9ff
eshan <daruleshan1122@gmail.com>
2025-07-28 04:13:07 -0700

    Merge remote changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,5 @@
 
 @extends("master")
-=======
-<ul>
-    <li><a href="">Home</a> </li>
-    <li><a href="">About</a></li>
-    <li><a href="">Contact</a></li>
-
-</ul>
-
+@section('title')
+    Welcome
+    @endsection
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -29,7 +29,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <h1 class="display-4 text-center text-white">Welcome to Laravel</h1>
+                <h1 class="display-4 text-center text-white">@yield('title')</h1>
             </div>
         </div>
     </div>
